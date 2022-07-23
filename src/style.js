43d79968bff71b38155fa80b47946422d503df20
ramda <91455804+gnfnfnr@users.noreply.github.com>
2022-07-23 12:55:40 +0900

    Revert "Add Community"
    This reverts commit 33f6b1f20c2ebb85e789071820a4fdf994839a25.

--- src/style.js
+++
@@ -1,9 +0,0 @@
-import { createGlobalStyle } from "styled-components";
-import reset from "styled-reset";
-export const GlobalStyle = createGlobalStyle`
-${reset}
-    body{
-        background-color: #f2f2f2;
-        font-family: Noto-sans;
-    }
-`;
